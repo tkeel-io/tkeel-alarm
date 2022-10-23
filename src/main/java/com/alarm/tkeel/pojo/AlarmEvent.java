6528bd2887beb05e5fcb3fbfb9575ae041403625
liufeng <liuf@kubesphere.io>
2022-10-23 19:08:15 +0800

    feat: add event

---
+++ src/main/java/com/alarm/tkeel/pojo/AlarmEvent.java
@@ -0,0 +1,29 @@
+package com.alarm.tkeel.pojo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @Author neov
+ * @Date 2022/10/23/14:45
+ */
+@Data
+public class AlarmEvent implements Serializable {
+    private Long eventId;
+    private String alarmEventId;
+    private Long ruleId;
+    private String tenantId;
+    private String alarmName;
+    private Integer alarmLevel;
+    private Integer alarmSource;
+    private Integer alarmType;
+    private String objectId;
+    private String telemetryId;
+    private Long startTime;
+    private Long handTime;
+    private Integer alarmStatus;
+    private String alarmValue;
+    private String alarmDesc;
+
+}
